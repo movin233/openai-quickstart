b36ad067417808faea2fb51d5fae90d61f414004
MOVIN PANG (POIPCC-LOSCP-ISD-OOCLL/ZHA) <PANGMO@OOCL.COM>
2024-07-27 19:46:22 +0800

    [feat]: perfect clothes sales chatbot and readme.md

--- README-Movin.md
+++ README-Movin.md
@@ -2,9 +2,51 @@
 
 ## ChatGPT 最朴素的 Prompt技巧：将大模型当做你的工作伙伴 实践作业
 
-本次作业的目标是让你练习使用ChatGPT进行文本生成。作业内容是生成一份学习日报，让chatgpt解读上传的课程pdf文件，总结课程学习内容。
+本次作业的目标是练习使用ChatGPT进行文本生成。作业内容是生成一份学习日报，让chatgpt解读上传的课程pdf文件，总结课程学习内容。
 
 作业link如下，可直接打开查看：
 https://chatgpt.com/share/e44047e7-079d-4659-94fb-a00a2e9a3b5a
 
+# Day2
+
+## GPT-4V/translator/langchain chains实践作业
+
+1.上传自己手写的图片到images文件夹，可命名为gpt-4v.jpg；
+2.修改query_base64_image_description的方法描述，分析gpt-4v.jpg图片；
+3.输出分析结果，并使用函数对输出进行渲染，渲染成Markdown格式使结果更易读；。
+
+### 作业结果如下图：
+### 手写图片
+![](./openai_api/images/handwriting_2.jpg)
+### 分析结果
+![](./homework_results/gpt-4v-0.png)
+### 格式化结果
+![](./homework_results/gpt-4v-1.png)
+
+
+ai translator中，添加2种以上的其他语言对翻译
+### 作业结果如下图，具体可看translator的readme文件：
+### 英翻中
+![](./homework_results/英翻中.png)
+### 英翻韩
+![](./homework_results/英翻韩.png)
+
+
+扩展chains，使其支持生物、计算机和汉语文学老师等学科的提示词模板及对应 Chains问答
+
+### 作业结果如下图：
+### 提示词模板
+![](./homework_results/langchain-0.png)
+### Chains问答
+![](./homework_results/langchain-1.png)
+
+# Day3
+
+## GPT-4V 实践作业
+
+1.自行生成一个新的产品介绍知识库，可搜索使用一个产品的行业数据，也可自建一个产品的真实数据。参考“房产销售聊天机器人”，开发一个“**销售聊天机器人”；
+2.在"**销售聊天机器人"中，使用向量数据库进行检索回答，如果向量数据库检索不到问题答案时，能够通过一个prompt来回答这个问题，而不是直接告诉用户我是一个AI机器人。
+
+### 作业结果如下图：
+![](./homework_results/clothes_sales_chatbot.png)
 
--- langchain/clothes_sales_chatbot/sales_chatbot.py
+++ langchain/clothes_sales_chatbot/sales_chatbot.py
@@ -4,6 +4,8 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+import openai
 
 import os
 
@@ -12,6 +14,21 @@ os.environ["OPENAI_API_KEY"] = "sk-38rkK0xdFDDMClm688CbCb0204F64d3d874066CbEf2fE
 os.environ['OPENAI_BASE_URL'] = 'https://api.xiaoai.plus/v1'
 
 def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
+    # 读取向量数据库
+    with open("real_estate_sales_data.txt",encoding='utf-8') as f:
+     real_estate_sales = f.read()
+    # 使用 CharacterTextSplitter 来进行文本分割
+    text_splitter = CharacterTextSplitter(        
+    separator = r'\d+\.',
+    chunk_size = 100,
+    chunk_overlap  = 0,
+    length_function = len,
+    is_separator_regex = True,)
+    docs = text_splitter.create_documents([real_estate_sales])
+    # 使用 Faiss 作为向量数据库，持久化存储衣服销售问答对（QA-Pair）
+    db = FAISS.from_documents(docs, OpenAIEmbeddings())
+    db.save_local("real_estates_sale")
+    # 加载 FAISS 向量数据库已有结果
     db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(),allow_dangerous_deserialization=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     
@@ -27,19 +44,29 @@ def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
 def sales_chat(message, history):
     print(f"[message]{message}")
     print(f"[history]{history}")
-    # TODO: 从命令行参数中获取
-    enable_chat = True
 
     ans = SALES_BOT({"query": message})
-    # 如果检索出结果，或者开了大模型聊天模式
+
+    # 如果检索出结果
     # 返回 RetrievalQA combine_documents_chain 整合的结果
-    if ans["source_documents"] or enable_chat:
+    if ans["source_documents"]:
         print(f"[result]{ans['result']}")
         print(f"[source_documents]{ans['source_documents']}")
         return ans["result"]
-    # 否则输出套路话术
+    # 否则就重新調用ChatOpenAI大语言模型進行回答
     else:
-        return "这个问题我要问问领导"
+        # 创建 system prompt
+        chat_input = [
+        {"role": "system", "content": "You are a knowledgeable salesperson for clothes. Please answer the questions as if you are talking to a customer and maintain the persona of a human sales assistant throughout the conversation."},
+        {"role": "user", "content": message}]
+
+        # 使用模型生成回答
+        chat_response = openai.chat.completions.create(model="gpt-3.5-turbo", messages=chat_input)
+        print(chat_response)
+      
+        personalized_response = chat_response.choices[0].message.content
+
+        return personalized_response
     
 
 def launch_gradio():
